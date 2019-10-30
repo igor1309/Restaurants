a6b4942ca15d45df34fc981e5553eaf01e10ca13
Igor Malyarov <igor1309@gmail.com>
2019-10-30 17:40:07 +0300

    removed extra public

--- Restaurants/Extensions/String+Extensions.swift
+++ Restaurants/Extensions/String+Extensions.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 public extension String {
+    func removingPrefix(_ prefix: String) -> String {
+        guard hasPrefix(prefix) else { return self }
+        return String(dropFirst(prefix.count))
+    }
+
+    func removingSuffix(_ suffix: String) -> String {
+        guard hasSuffix(suffix) else { return self }
+        return String(dropLast(suffix.count))
+    }
+
+    func appendingSuffixIfNeeded(_ suffix: String) -> String {
+        guard !hasSuffix(suffix) else { return self }
+        return appending(suffix)
+    }
+}
+
+public extension String {
     var isNotEmpty: Bool {
         !isEmpty
     }
